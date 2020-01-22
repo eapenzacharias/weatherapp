7b71f62979cadc4b1c4dc193a6ca6d792fb1ae1b
eapenzacharias <eapenzac@gmail.com>
2020-01-22 02:14:36 +0100

    unsplash background img api added

--- src/dom.js
+++ src/dom.js
@@ -1,4 +1,5 @@
 import { reportWeather } from './fetchdata';
+import Unsplash, { toJson } from 'unsplash-js';
 
 function tempC(weather) {
   document.getElementById('main-temp').innerHTML = `${weather.tempC} &#8451;`;
@@ -14,6 +15,19 @@ function tempF(weather) {
   document.getElementById('max-temp').innerHTML = `${weather.tempMaxF} &#8457;`;
 }
 
+async function imgUnsplash(text) {
+  const UACCESS = process.env.UACCESS;
+  const unsplash = new Unsplash({
+    accessKey: `${UACCESS}`,
+  });
+  const bg = await unsplash.search.photos(`${text}+sky`, 1, 1);
+  const bgJson = await bg.json();
+  document.body.style.backgroundImage = `url(${bgJson.results[0].urls.full})`;
+  const credit = document.getElementById('img-txt');
+  credit.innerHTML = `Photo by ${bgJson.results[0].user.first_name} ${bgJson.results[0].user.last_name} from Unsplash`;
+  credit.href = `${bgJson.results[0].links.html}`
+}
+
 async function displayInit(city) {
   const weather = await reportWeather(city);
 
@@ -22,6 +36,8 @@ async function displayInit(city) {
   document.getElementById('weather-icon').innerHTML = icon.outerHTML;
   document.getElementById('weather-report').innerHTML = weather.text;
   document.getElementById('location').innerHTML = `${weather.city}, ${weather.country}`;
+
+  imgUnsplash(weather.text);
 
   tempC(weather);
   document.getElementById('in-c').onclick = function callTempC() {
